05e741799874f7b03d9547354ddd8f786176914d
michael <michael.raposo@hotmail.com>
2026-04-29 20:59:56 -0300

    Iniciando Projeto para os Testes de Unidade

---
+++ testesDeUnidade/src/main/java/com/MichaelAlcanatara/testesDeUnidade/TestesDeUnidadeApplication.java
@@ -0,0 +1,13 @@
+package com.MichaelAlcanatara.testesDeUnidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestesDeUnidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestesDeUnidadeApplication.class, args);
+	}
+
+}
---
+++ testesDeUnidade/src/test/java/com/MichaelAlcanatara/testesDeUnidade/TestesDeUnidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MichaelAlcanatara.testesDeUnidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesDeUnidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
