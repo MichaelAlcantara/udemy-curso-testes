6bb7e26860043bbeea9040b1091b04b94250e639
michael <michael.raposo@hotmail.com>
2026-04-29 21:51:31 -0300

    Criação da API de planetas

---
+++ testesDeUnidade/src/main/java/com/MichaelAlcanatara/testesDeUnidade/domain/PlanetService.java
@@ -0,0 +1,19 @@
+package com.MichaelAlcanatara.testesDeUnidade.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlanetService {
+	
+	private PlanetRepository planetRepository;
+	
+	public PlanetService(PlanetRepository planetRepository) {
+		this.planetRepository = planetRepository;
+	}
+
+	public Planet create(Planet planet) {
+		return planetRepository.save(planet);
+	}
+
+}
