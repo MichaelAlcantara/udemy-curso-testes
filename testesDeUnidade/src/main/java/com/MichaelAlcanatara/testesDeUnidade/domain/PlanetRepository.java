c8194f9980d143a66e69725fea626fd348df6966
michael <michael.raposo@hotmail.com>
2026-05-06 15:45:00 -0300

    Find All

--- testesDeUnidade/src/main/java/com/MichaelAlcanatara/testesDeUnidade/domain/PlanetRepository.java
+++ testesDeUnidade/src/main/java/com/MichaelAlcanatara/testesDeUnidade/domain/PlanetRepository.java
@@ -1,11 +1,15 @@
 package com.MichaelAlcanatara.testesDeUnidade.domain;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Example;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PlanetRepository extends CrudRepository<Planet, Long>{
+public interface PlanetRepository extends CrudRepository<Planet, Long> {
 
 	Optional<Planet> findByName(String name);
+
+	<S extends Planet> List<S> findAll(Example<S> example);
 
 }
