8a1a9ce14331da7c64765a8bcf2801b448cdaf90
michael <michael.raposo@hotmail.com>
2026-05-06 15:39:45 -0300

    Find By Id

--- testesDeUnidade/src/main/java/com/MichaelAlcanatara/testesDeUnidade/domain/PlanetService.java
+++ testesDeUnidade/src/main/java/com/MichaelAlcanatara/testesDeUnidade/domain/PlanetService.java
@@ -1,19 +1,24 @@
 package com.MichaelAlcanatara.testesDeUnidade.domain;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class PlanetService {
-	
+
 	private PlanetRepository planetRepository;
-	
+
 	public PlanetService(PlanetRepository planetRepository) {
 		this.planetRepository = planetRepository;
 	}
 
 	public Planet create(Planet planet) {
 		return planetRepository.save(planet);
+	}
+
+	public Optional<Planet> get(Long id) {
+		return planetRepository.findById(id);
 	}
 
 }
--- testesDeUnidade/src/main/java/com/MichaelAlcanatara/testesDeUnidade/web/PlanetController.java
+++ testesDeUnidade/src/main/java/com/MichaelAlcanatara/testesDeUnidade/web/PlanetController.java
@@ -3,6 +3,8 @@ package com.MichaelAlcanatara.testesDeUnidade.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,14 +16,20 @@ import com.MichaelAlcanatara.testesDeUnidade.domain.PlanetService;
 @RestController
 @RequestMapping("/planets")
 public class PlanetController {
-	
+
 	@Autowired
 	private PlanetService planetService;
-	
+
 	@PostMapping
-	public ResponseEntity<Planet> create (@RequestBody Planet plant){
+	public ResponseEntity<Planet> create(@RequestBody Planet plant) {
 		Planet planedCreated = planetService.create(plant);
 		return ResponseEntity.status(HttpStatus.CREATED).body(planedCreated);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Planet> get(@PathVariable("id") Long id) {
+		return planetService.get(id).map(planet -> ResponseEntity.ok(planet))
+				.orElseGet(() -> ResponseEntity.notFound().build());
 	}
 
 }
