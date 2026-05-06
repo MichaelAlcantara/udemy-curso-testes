4946002f0ea8be39c375d2f67868a5f8acc2708d
michael <michael.raposo@hotmail.com>
2026-05-06 16:05:20 -0300

    Teste do Find By Id

--- testesDeUnidade/src/test/java/com/MichaelAlcanatara/testesDeUnidade/domain/PlanetServiceTest.java
+++ testesDeUnidade/src/test/java/com/MichaelAlcanatara/testesDeUnidade/domain/PlanetServiceTest.java
@@ -6,6 +6,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,11 +18,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 public class PlanetServiceTest {
 
-	//@Autowired
+	// @Autowired
 	@InjectMocks
 	private PlanetService planetService;
-	
-	//@MockBean
+
+	// @MockBean
 	@Mock
 	private PlanetRepository planetRepository;
 
@@ -39,12 +41,31 @@ public class PlanetServiceTest {
 		// A -> Assert
 		assertThat(sut).isEqualTo(PLANET);
 	}
-	
+
 	@Test
 	public void createPlanet_WithInvalidData_ThrowsException() {
 		
 		when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
 		
 		assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
+	}
+
+	@Test
+	public void getPlanet_ByExistingId_ReturnsPlanet() {
+		when(planetRepository.findById(1L)).thenReturn(Optional.empty());
+
+	    Optional<Planet> sut = planetService.get(1L);
+
+	    assertThat(sut).isEmpty();
+	}
+
+	@Test
+	public void getPlanet_ByUnexistingId_ReturnsEmpty() {
+		when(planetRepository.findByName(PLANET.getName())).thenReturn(Optional.of(PLANET));
+
+	    Optional<Planet> sut = planetService.getByName(PLANET.getName());
+
+	    assertThat(sut).isNotEmpty();
+	    assertThat(sut.get()).isEqualTo(PLANET);
 	}
 }
