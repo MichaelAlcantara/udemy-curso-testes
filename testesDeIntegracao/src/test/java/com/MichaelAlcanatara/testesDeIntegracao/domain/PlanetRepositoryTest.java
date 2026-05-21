3634730abaa2b8cf9cd859beab9a20ec56a7efbb
michael <michael.raposo@hotmail.com>
2026-05-21 20:15:06 -0300

    Testando o cenário de sucesso no repository

---
+++ testesDeIntegracao/src/test/java/com/MichaelAlcanatara/testesDeIntegracao/domain/PlanetRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.MichaelAlcanatara.testesDeIntegracao.domain;
+
+import static com.MichaelAlcanatara.testesDeIntegracao.commom.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+@DataJpaTest //Cria um banco de dados em mémoria, só para testa
+public class PlanetRepositoryTest {
+	
+	@Autowired
+	private PlanetRepository planetRepository;
+	
+	@Autowired
+	private TestEntityManager testEntityManager; //Usado para fazer a busca sem ser no repositorio
+	
+	@Test
+	public void createPlanet_WithValidData_ReturnsPlanet() {
+		Planet planet = planetRepository.save(PLANET);
+		
+		Planet sut = testEntityManager.find(Planet.class, planet.getId());
+		
+		assertThat(sut).isNotNull();
+		assertThat(sut.getName()).isEqualTo(PLANET.getName());
+		assertThat(sut.getClimate()).isEqualTo(PLANET.getClimate());
+		assertThat(sut.getTerrain()).isEqualTo(PLANET.getTerrain());
+	}
+
+}
